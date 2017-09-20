d8589e39bff3aa3fd4fc3bcf11f278f7f898c69e
Homer White <homerhanumat@gmail.com>
2017-09-20 16:38:19 -0400

    maybe forgot to include the very latest revisionin previous commit

--- perl/collecthomework.pl
+++ perl/collecthomework.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+##!/usr/bin/perl -w
 
 use strict;
 use File::Find;
@@ -104,8 +104,8 @@ while (<$INPUTFILE>) {
 
                 $destinationFolder = $destinationFolder . "/";
                 system ("cp -f $projectFile $destinationFolder");
-                system("sudo chmod -R 775 $destinationFolder");
-		$summaryLine = $summaryLine . "\n $inputLine submitted file: $projectFile";		
+		$summaryLine = $summaryLine . "\n $inputLine submitted file: $projectFile";	
+		system("sudo chmod -R 775 $destinationFolder");
 
 	}
 
